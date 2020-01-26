04b33081d46c2bdb1ad62a97922417e0bcbaf1d1
Dawid <david.d.bialy@gmail.com>
2020-01-26 22:36:06 +0000

    - configured storing identities in the database

---
+++ DutchTreat/Data/Migrations/20200126220145_Identity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DutchTreat.Migrations
+{
+    public partial class Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2020, 1, 26, 22, 1, 43, 654, DateTimeKind.Utc).AddTicks(7234));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2020, 1, 18, 23, 47, 18, 496, DateTimeKind.Utc).AddTicks(4398));
+        }
+    }
+}
