af71bb63b07572c91b4945289dd55ef0b770d085
Dawid <david.d.bialy@gmail.com>
2020-02-05 23:38:02 +0000

    - Angular app has been integreated in the project

--- DutchTreat/ClientApp/app/app.component.ts
+++ DutchTreat/ClientApp/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'the-shop',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
+ 
     <div style="text-align:center" class="content">
       <h1>
         Welcome to {{title}}!
