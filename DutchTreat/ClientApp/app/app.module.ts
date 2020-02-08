9622fe8e6404cbaffc7696935dfd5aa8aed75be1
Dawid <david.d.bialy@gmail.com>
2020-02-08 23:59:41 +0000

    - Fixed a typo in app.module.ts

--- DutchTreat/ClientApp/app/app.module.ts
+++ DutchTreat/ClientApp/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ProductList } from '.shop/productList.component';
+import { ProductList } from './shop/productList.component';
 
 @NgModule({
   declarations: [
