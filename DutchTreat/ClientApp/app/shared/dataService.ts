93e2f0eafafbfe18b935f4154a3a137ffac52dfc
Dawid <david.d.bialy@gmail.com>
2020-02-10 22:25:30 +0000

    - dataService now calls the API to return the products

--- DutchTreat/ClientApp/app/shared/dataService.ts
+++ DutchTreat/ClientApp/app/shared/dataService.ts
@@ -1,14 +1,21 @@
-﻿export class DataService {
+﻿import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from 'rxjs/operators';
 
-    public products = [{
-        title: "First Product",
-        price: 19.99
-    }, {
-        title: "Second Product",
-        price: 29.99
-    }, {
-        title: "Third Product",
-        price: 14.99
-        }];
+@Injectable()
+export class DataService {
+
+    constructor(private http: HttpClient) { }
+
+    public products = [];
+
+    loadProducts() {
+       return this.http.get("/api/products")
+            .pipe(
+                map((data: any[]) => {
+                    this.products = data;
+                    return true;
+                }));
+    }
 
 }
